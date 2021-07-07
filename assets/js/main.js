b6f2f88ce0e465a873d69db300c3618e30bba46d
Adriana Mucciolo <drishaolin@gmail.com>
2021-07-07 18:40:38 -0300

    populate global data

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,13 +2,36 @@ function fetchJson(url) {
   return fetch(url).then((resp) => resp.json());
 }
 
+function formatNumber(n) {
+  return new Intl.NumberFormat("pt-br").format(n);
+}
+
+function renderData(id, kpi) {
+  return (document.getElementById(id).innerHTML = formatNumber(kpi));
+}
+
 const baseURL = "https://api.covid19api.com";
 
 async function getCovidData() {
   let data = await fetchJson(`${baseURL}/summary`);
   let global = data.Global;
   let countriesArray = data.Countries;
-  console.log("Global", data.Global);
-  console.log("Countries", data.Countries);
+  return {global, countriesArray};
 }
-getCovidData();
+
+(async function populateGlobalData() {
+  ({global} = await getCovidData());
+  renderData("confirmed", global.TotalConfirmed);
+  renderData("death", global.TotalDeaths);
+  renderData("recovered", global.TotalRecovered);
+  let day = global.Date.slice(8, 10);
+  let month = global.Date.slice(5, 7);
+  let year = global.Date.slice(0, 4);
+  let currentDate = `${day}/${month}/${year}`;
+
+  document.getElementById("actives").innerHTML = "Data de Atualização";
+  document.getElementById(
+    "active"
+  ).innerHTML = `${currentDate} - ${global.Date.slice(11, 16)}`;
+})();
+
