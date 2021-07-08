6971912a2c4c4e1c7ae1edfef9cb803ef2927e75
Adriana Mucciolo <drishaolin@gmail.com>
2021-07-08 19:53:42 -0300

    calculate and populate diary data by country

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+//-------------AUXILIAR FUNCTIONS-------------
 function fetchJson(url) {
   return fetch(url).then((resp) => resp.json());
 }
@@ -6,11 +7,30 @@ function formatNumber(n) {
   return new Intl.NumberFormat("pt-br").format(n);
 }
 
-function renderData(id, kpi) {
-  return (document.getElementById(id).innerHTML = formatNumber(kpi));
+function subtractDay(selectedDate) {
+  let date = new Date(selectedDate);
+  let dateUTC = date.getTimezoneOffset() / 60;
+  let milisecondsByDay = 1000 * 60 * 60 * (24 - dateUTC);
+  let deltaDate = new Date(date.getTime() - milisecondsByDay);
+  let year = deltaDate.getFullYear().toString();
+  let month = deltaDate.getMonth() + 1; //months starts in 0 (represents january)
+  month = month.toString().padStart(2, "0");
+  let day = deltaDate.getDate().toString().padStart(2, "0");
+  let deltaDateFormatted = `${year}-${month}-${day}`;
+  return deltaDateFormatted;
 }
 
+//-------------PRINCIPAL--------------
 const baseURL = "https://api.covid19api.com";
+
+function renderData(id, kpi, text) {
+  if(text) return (document.getElementById(id).innerHTML = text + " - " + formatNumber(kpi));
+  else return (document.getElementById(id).innerHTML = formatNumber(kpi));
+}
+
+function renderDiaryValues(id, kpi, text) {
+    
+}
 
 async function getCovidData() {
   let data = await fetchJson(`${baseURL}/summary`);
@@ -29,7 +49,7 @@ async function getCovidData() {
   let year = global.Date.slice(0, 4);
   let currentDate = `${day}/${month}/${year}`;
 
-  document.getElementById("actives").innerHTML = "Data de Atualização";
+  document.getElementById("actives-title").innerHTML = "Data de Atualização";
   document.getElementById(
     "active"
   ).innerHTML = `${currentDate} - ${global.Date.slice(11, 16)}`;
@@ -49,14 +69,21 @@ async function populateComboCountries() {
 
 async function populateCountriesData() {
     let selectedDate = document.getElementById("today").value;
+    console.log(selectedDate);
     let selectedCountry = document.getElementById("combo").value;
-    let startDate = `${selectedDate}T00:00:00Z`;
+    let startDate = `${subtractDay(selectedDate)}T00:00:00Z`;
     let endDate = `${selectedDate}T23:59:59Z`;
     let country = await fetchJson(`${baseURL}/country/${selectedCountry}?from=${startDate}&to=${endDate}`);
       
-  renderData("confirmed", country[0].Confirmed);
-  renderData("death", country[0].Deaths);
-  renderData("recovered", country[0].Recovered);
-  document.getElementById("actives").innerHTML = "Ativos";
-  renderData("active", country[0].Active);
+  renderData("confirmed", country[1].Confirmed);
+  renderData("death", country[1].Deaths);
+  renderData("recovered", country[1].Recovered);
+  document.getElementById("actives-title").innerHTML = "Ativos";
+  renderData("active", country[1].Active);
+  
+  renderData("diary-confirmed", country[1].Confirmed-country[0].Confirmed, "Diário");
+  renderData("diary-death", country[1].Deaths-country[0].Deaths, "Diário");
+  renderData("diary-recovered", country[1].Recovered-country[0].Recovered, "Diário");
+ renderData("diary-active", country[1].Active-country[0].Active, "Diário");
 }
+
